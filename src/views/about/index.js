c9816352fe3babb7287768fa422ed77ce33b3e24
Rosanna <202148302+popsnoret@users.noreply.github.com>
2025-11-10 19:20:52 +0100

    header and about

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,10 +1,18 @@
 export default function about() {
- const about = document.createElement('div')
- about.classList.add('about')
+  const about = document.createElement("div");
+  about.classList.add("about", "flex", "justify-center", "items-center", "min-h-screen");
 
- about.innerHTML = `
-    <h1>Hello</h1>
- `
+  about.innerHTML = `
+    <div class="backdrop-blur-md bg-gray-900/60 p-10 rounded-4xl shadow-2xl max-w-2xl text-center">
+      <h1 class="font-[Great_Vibes] text-5xl mb-4">About the calendar</h1>
+      <p class="text-lg text-gray-100 leading-relaxed mb-6">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, 
+        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+      </p>
+   
+    </div>
+  `;
 
- return about
+  return about;
 }
